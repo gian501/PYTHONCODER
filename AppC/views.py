f83e92f5177388f120d84b064122785e1c34a7dc
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-09 17:19:06 -0300

    otra mod

--- AppC/views.py
+++ AppC/views.py
@@ -3,8 +3,30 @@ from django.http import HttpResponse
 from AppC.models import Curso
 
 # Create your views here.
+'''
 def curso(self):
     curso = Curso(nombre = 'Desarrollo web', comision = '19881') #igual que el nombre de la clase en models
     curso.save()
     documentoTexto = f'--> Curso: {curso.nombre} comision: {curso.comision}'
     return HttpResponse(documentoTexto)
+'''
+
+def inicio(request):
+    #return render(request, 'AppCoder/inicio.html') # usar el nombre del template
+    return HttpResponse('Vista Inicio')
+
+def cursos(request):
+    #return render(request, 'AppCoder/cursos.html')
+    return HttpResponse('Vista cursos')
+
+def profesores(request):
+    #return render(request, 'AppCoder/profesores.html')
+    return HttpResponse('Vista profesores')
+
+def entregables(request):
+    #return render(request, 'AppCoder/entregables.html')
+    return HttpResponse('Vista entregables')
+
+def estudiantes(request):
+    #return render(request, 'AppCoder/estudiantes.html')
+    return HttpResponse('Vista estudiantes')
